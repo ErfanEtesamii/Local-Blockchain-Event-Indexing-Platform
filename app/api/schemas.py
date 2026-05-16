28cb0aa24b7ff6452f599a5bd97042178daeef85
Erfan Etesami <etesami.er@gmail.com>
2026-05-16 14:02:13 +0330

    refactor(api): align transfer schema with database model

--- app/api/schemas.py
+++ app/api/schemas.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
 from datetime import datetime
 from decimal import Decimal
 from typing import Optional
+
+from pydantic import BaseModel
+
 
 class TokenTransfer(BaseModel):
     id: int
